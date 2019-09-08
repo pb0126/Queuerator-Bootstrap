96393013c7ab084fbf150306c8f7f50188e6e069
pb0126 <prabhubisht0126@gmail.com>
2019-09-08 14:35:57 +0530

    home page background fixed, responsive navbar fixed

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 var i=0;
-function openWtab(){
+function open_res_sidenav(){
   if(i==0){
-    console.log(i);
     document.getElementById("nav-res-sidebar").style.display="block";
     document.getElementById("nav-res-list-ico").style.display="none";
     document.getElementById("nav-res-closebtn").style.display="block";
     i=1;
-    console.log(i);
   }
   else{
     document.getElementById("nav-res-sidebar").style.display="none";
